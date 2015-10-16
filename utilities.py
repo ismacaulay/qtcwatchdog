d103f93e05d33aa0666a1c988d096e8e33518f70
Ian Macaulay <ismacaul@gmail.com>
2015-10-15 22:54:24 -0600

    Implemented extension validator

--- utilities.py
+++ utilities.py
@@ -17,32 +17,3 @@ def all_files_and_dirs(path):
 
 def split_semicolon(s):
    return s.strip().split(';')
-
-def write_list(path, list_to_write):
-   with open(path, 'w') as f:
-      for i in list_to_write:
-         f.write(i + '\n')
-
-def append_line(path, line):
-   with open(path, 'a') as f:
-      f.write(line + '\n')
-
-def remove_line(path, line):
-   with open(path, 'r+') as f:
-      data = f.readlines()
-      f.seek(0)
-      for line in data:
-         if line.strip() != remove:
-            f.write(line)
-      f.truncate()
-
-def remove_and_append(path, remove, append):
-   print 'utilities::remove_and_append: r:' + remove + ' a: ' + append
-   with open(path, 'r+') as f:
-      data = f.readlines()
-      f.seek(0)
-      for line in data:
-         if line.strip() != remove:
-            f.write(line)
-      f.write(append + '\n')
-      f.truncate()
