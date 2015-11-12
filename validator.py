fb1a6b3693c3b6adf7da7a5280142cfbb11cc917
Ian Macaulay <ian.s.macaulay@lmco.com>
2015-11-12 15:50:30 -0700

    Fixed validator when empty string passed in

--- validator.py
+++ validator.py
@@ -3,8 +3,13 @@ import re
 
 class RegexValidator(object):
    def __init__(self, pattern, excludes):
+      if pattern == '':
+         pattern = '.*' # match anything
+      if excludes == '':
+         excludes = 'a^' # match nothing
+
       self._pattern = re.compile(pattern)
       self._excludes = re.compile(excludes)
 
    def is_valid(self, string):
-      return self._pattern.match(string) and not self._excludes.match(string):
+      return self._pattern.match(string) and not self._excludes.match(string)
