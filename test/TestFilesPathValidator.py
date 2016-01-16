11ab8b05c3741b0eaa1b623c18200ce6771c7f8f
Ian Macaulay <ismacaul@gmail.com>
2016-01-16 10:41:25 -0700

    Created a new validator that can exclude specific paths separate from the settings regex

---
+++ test/TestFilesPathValidator.py
@@ -0,0 +1,23 @@
+import unittest
+from validator import FilesPathValidator
+
+
+class TestFilesPathValidator(unittest.TestCase):
+    def test_excludedPathsAreInvalid(self):
+        excluded_paths = ['hello', 'world']
+
+        patient = FilesPathValidator(excluded_paths, '', '')
+
+        self.assertFalse(patient.is_valid('hello'))
+        self.assertFalse(patient.is_valid('world'))
+
+    def test_excludedPathsAreInvalidEvenIfTheyMatchTheRegex(self):
+        excluded_paths = ['hello', 'world']
+
+        patient = FilesPathValidator(excluded_paths, 'hello*', '')
+
+        self.assertFalse(patient.is_valid('hello'))
+        self.assertTrue(patient.is_valid('helloWorld'))
+
+if __name__ == '__main__':
+    unittest.main()
--- validator.py
+++ validator.py
@@ -14,3 +14,16 @@ class RegexValidator(object):
 
     def is_valid(self, string):
         return self._pattern.match(string) and not self._excludes.match(string)
+
+
+class FilesPathValidator(RegexValidator):
+    def __init__(self, excluded_paths, pattern_regex, excludes_regex):
+        self._excluded_paths = excluded_paths
+
+        super(FilesPathValidator, self).__init__(pattern_regex, excludes_regex)
+
+    def is_valid(self, string):
+        if string not in self._excluded_paths:
+            return super(FilesPathValidator, self).is_valid(string)
+        return False
+
