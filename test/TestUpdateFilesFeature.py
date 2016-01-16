119b51dfb8ca32a8d09e7b4cb020dc68ca9ea863
Ian Macaulay <ismacaul@gmail.com>
2016-01-16 00:37:39 -0700

    Added an AAT for regex, and did some clean up

--- test/TestUpdateFilesFeature.py
+++ test/TestUpdateFilesFeature.py
@@ -32,22 +32,16 @@ class TestUpdateFilesFeature(fake_filesystem_unittest.TestCase):
 
     def test_willAddFilePathToFilesFile(self):
         self.setup_project_directory()
-
-        watchdog = QtcWatchdog(self.project_settings)
-        watchdog.start()
+        self.create_and_start_watchdog()
 
         added_files = self.create_some_files()
 
-        (files_contains, msg) = self.files_contains_paths(added_files)
-        self.assertTrue(files_contains, msg)
-        (files_contains, msg) = self.files_contains_paths(self.initial_files)
-        self.assertTrue(files_contains, msg)
+        self.verify_files_contains_paths(added_files)
+        self.verify_files_contains_paths(self.initial_files)
 
-    def test_WillRemoveFilePathFromFilesFile(self):
+    def test_willRemoveFilePathFromFilesFile(self):
         self.setup_project_directory()
-
-        watchdog = QtcWatchdog(self.project_settings)
-        watchdog.start()
+        self.create_and_start_watchdog()
 
         added_files = self.create_some_files()
 
@@ -56,62 +50,111 @@ class TestUpdateFilesFeature(fake_filesystem_unittest.TestCase):
 
         self.remove_some_files([removed_added_file, removed_initial_file])
 
-        (files_contains, msg) = self.files_contains_paths([removed_added_file])
-        self.assertFalse(files_contains, msg)
-        (files_contains, msg) = self.files_contains_paths([removed_initial_file])
-        self.assertFalse(files_contains, msg)
-        (files_contains, msg) = self.files_contains_paths([added_files[0], self.initial_files[0]])
+        self.verify_files_does_not_contain_path(removed_added_file)
+        self.verify_files_does_not_contain_path(removed_initial_file)
+        self.verify_files_contains_paths([added_files[0], self.initial_files[0]])
+
+    def test_willOnlyIncludeFilesThatMatchTheRegex(self):
+        self.setup_project_directory()
+        self.setup_project_files_regex('^.*\.(cxx|h|py)$')
+        self.create_and_start_watchdog()
+
+        files = [
+            os.path.join(self.project_settings['project_path'], 'helloWorld.cxx'),
+            os.path.join(self.project_settings['project_path'], 'helloWorld.doc'),
+            os.path.join(self.project_settings['project_path'], 'helloWorld.py'),
+            os.path.join(self.project_settings['project_path'], 'helloWorld.h'),
+            os.path.join(self.project_settings['project_path'], 'helloWorld.txt'),
+            os.path.join(self.project_settings['project_path'], 'helloWorld.cxx.bak'),
+            os.path.join(self.project_settings['project_path'], 'helloWorld.cxx.1234'),
+        ]
+        self.create_files(files)
+
+        expected_files = [
+            os.path.join(self.project_settings['project_path'], 'helloWorld.cxx'),
+            os.path.join(self.project_settings['project_path'], 'helloWorld.h'),
+            os.path.join(self.project_settings['project_path'], 'helloWorld.py'),
+            os.path.join(self.project_settings['project_path'], 'initial_file.cxx'),
+            os.path.join(self.project_settings['project_path'], 'initial_file.h'),
+        ]
+        expected_not_in_files = [
+            os.path.join(self.project_settings['project_path'], 'helloWorld.txt'),
+            os.path.join(self.project_settings['project_path'], 'helloWorld.doc'),
+            os.path.join(self.project_settings['project_path'], 'initial_file.txt'),
+            os.path.join(self.project_settings['project_path'], 'helloWorld.cxx.bak'),
+            os.path.join(self.project_settings['project_path'], 'helloWorld.cxx.1234'),
+        ]
+        self.verify_files_contains_paths(expected_files)
+        for f in expected_not_in_files:
+            self.verify_files_does_not_contain_path(f)
+
+    def verify_files_contains_paths(self, paths):
+        (files_contains, msg) = self.files_contains_paths(paths)
         self.assertTrue(files_contains, msg)
+
+    def verify_files_does_not_contain_path(self, path):
+        (files_contains, msg) = self.files_contains_paths([path])
+        self.assertFalse(files_contains, msg)
+
+    def files_contains_paths(self, paths):
+        with open(self.files_file) as f:
+            lines = [f.strip('\n') for f in f.readlines()]
+            for path in paths:
+                if path not in lines:
+                    return False, 'files contains path: {}'.format(path)
+        return True, 'All paths in files file.'
+
+    def create_and_start_watchdog(self):
+        watchdog = QtcWatchdog(self.project_settings)
+        watchdog.start()
 
     def setup_project_directory(self):
         self.project_settings = {
             'project_name': 'watchdog',
             'project_path': os.path.relpath('/project/watchdog'),
+            'files': {},
+            'includes': {},
         }
+        self.files_file = os.path.join(self.project_settings['project_path'], 'watchdog.files')
+        self.includes_file = os.path.join(self.project_settings['project_path'], 'watchdog.includes')
 
         os.makedirs(self.project_settings['project_path'])
-        self.fs.CreateFile(os.path.relpath('/project/watchdog/watchdog.files'))
-        self.fs.CreateFile(os.path.relpath('/project/watchdog/watchdog.includes'))
+        self.fs.CreateFile(self.files_file)
+        self.fs.CreateFile(self.includes_file)
 
         self.initial_files = [
-            os.path.relpath('/project/watchdog/initial_file1.txt'),
-            os.path.relpath('/project/watchdog/initial_file2.txt'),
-            os.path.relpath('/project/watchdog/initial_file3.txt'),
+            os.path.join(self.project_settings['project_path'], 'initial_file.txt'),
+            os.path.join(self.project_settings['project_path'], 'initial_file.cxx'),
+            os.path.join(self.project_settings['project_path'], 'initial_file.h'),
         ]
-        self.create_files(self.initial_files)
+        for f in self.initial_files:
+            self.fs.CreateFile(f)
+
+    def setup_project_files_regex(self, regex):
+        self.project_settings['files']['regex'] = regex
 
     def save_updater(self, project_path_arg, updater_arg):
         self.file_updater = updater_arg
         return ProjectWatcher(project_path_arg, updater_arg)
 
-    @staticmethod
-    def files_contains_paths(paths):
-        with open(os.path.relpath('/project/watchdog/watchdog.files')) as f:
-            lines = [f.strip('\n') for f in f.readlines()]
-            for path in paths:
-                if path not in lines:
-                    return False, path
-        return True, ''
-
-    def create_some_files(self):
-        files = [
-            os.path.relpath('/project/watchdog/new_file1.txt'),
-            os.path.relpath('/project/watchdog/new_file2.txt'),
-            os.path.relpath('/project/watchdog/new_file3.txt'),
-        ]
+    def create_files(self, files):
         for f in files:
             self.fs_observer.create_file(f)
         self.file_updater.update_files()
+
+    def create_some_files(self):
+        files = [
+            os.path.join(self.project_settings['project_path'], 'new_file1.txt'),
+            os.path.join(self.project_settings['project_path'], 'new_file2.txt'),
+            os.path.join(self.project_settings['project_path'], 'new_file3.txt'),
+        ]
+        self.create_files(files)
         return files
 
     def remove_some_files(self, files):
         for f in files:
             self.fs_observer.remove_file(f)
         self.file_updater.update_files()
-
-    def create_files(self, files):
-        for f in files:
-            self.fs.CreateFile(f)
 
 
 class MockObserver(object):
